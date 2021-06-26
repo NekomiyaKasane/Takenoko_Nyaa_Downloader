f78d7693d1862e95604ee4d7004dfaef49ea815f
李一喆 <liyizhe@zwcad.com>
2021-06-26 18:12:37 +0800

    fix anonymous bug

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import requests
 import time
 import json
 import re
+import os, sys
 import pandas as pd
 from pyquery import PyQuery as pq
 
@@ -10,6 +11,9 @@ Output_CSV = True
 Output_name = ('out.csv', 'out_bad.csv', 'out_passed.csv')
 Domain = 'https://nyaa.si'
 Retry = 5
+
+allowed_author = ['jptv']
+allowed_category = []
 
 
 def get_last_time():
@@ -42,15 +46,18 @@ def get_html(url):
 
 
 def get_author(href_more):
-    more_body = get_html(href_more)
-    if more_body:
-        try:
-            more_body = more_body.find('.panel-body')
+    try:
+        more_body = get_html(href_more)
+        more_body = more_body.find('.panel-body')
+        if len(more_body[0][1][1]):
             author = more_body[0][1][1][0].text
             return author
-        except Exception as e:
-            print(e)
-    return False
+        else:
+            return "Anonymous"
+    except Exception as e:
+        print(e)
+        pass
+        return False
 
 
 def check_time(item):
@@ -71,12 +78,13 @@ def handle_table(trs, prefix, item_filter):
     data = []
     bad_data = []
     passed_data = []
-    end = True
     bad = False
     passed = False
+    end = False
     ite = 0
     num = len(trs)
     for item in trs:
+        time.sleep(0.1)
         ite += 1
         tds = item('td')
         href = prefix + tds[1][-1].attrib['href']
@@ -85,31 +93,29 @@ def handle_table(trs, prefix, item_filter):
         item = {
             'category': tds[0][-1].attrib['title'],
             'name': tds[1][-1].attrib['title'],
-            'author': author if author else "",
+            'author': author if author else "Nan",
             'size': tds[-5].text,
             'time': tds[-4].text,
             'completes': int(tds[-1].text),
-            'magnet': tds[2][-1].attrib['href']
+            'magnet': tds[-6][-1].attrib['href']
         }
-
         if not verify_item(item):
             bad_data.append(item)
             bad = True
-        if not item_filter(item):
+        elif not check_time(item):
+            end = True
+            print("\033[1;33mReached last time.\033[0m")
+            break
+        elif not item_filter(item):
             passed_data.append(item)
             passed = True
+        else:
+            data.append(item)
 
-        print("[{0}/{1}] {2} {3} {4}".format(ite, num, item['name'], "Bad" if bad else "", "Passed" if passed else ""))
+        print("\033[1;32m[{0}/{1}]\033[1;37m {2} \033[1;31m{3} \033[1;36m{4}\033[0m"
+              .format(ite, num, item['name'], "Bad" if bad else "", "Passed" if passed else ""))
 
-        if bad or passed:
-            bad = passed = False
-            continue
-
-        if not check_time(item):
-            end = True
-            break
-
-        data.append(item)
+        bad = passed = False
 
     data_frame = pd.DataFrame(data)
     bad_data_frame = pd.DataFrame(bad_data)
@@ -117,24 +123,24 @@ def handle_table(trs, prefix, item_filter):
     return data_frame, bad_data_frame, passed_frame, end
 
 
-def main():
+def main(argv=None):
     datas = pd.DataFrame(columns=('category', 'name', 'author', 'size', 'time', 'completes', 'magnet'))
     bad_datas = pd.DataFrame(columns=('category', 'name', 'author', 'size', 'time', 'completes', 'magnet'))
     passed_datas = pd.DataFrame(columns=('category', 'name', 'author', 'size', 'time', 'completes', 'magnet'))
 
-    for i in range(1):
-        print('Page {0}:'.format(i + 1))
+    for i in range(100):
+        print('\033[1;33mPage {0}:\033[0m'.format(i + 1))
         doc = get_html(Domain + '/?p={0}'.format(i + 1))
         if not doc:
             print("Failed to fetch {0}th page.".format(i + 1))
             break
         trs = list(doc.find('.torrent-list')('tbody')('tr').items())
         data, bad_data, passed_data, end = handle_table(trs, Domain, lambda item: True)
-        if end:
-            break
         datas = datas.append(data)
         bad_datas = bad_datas.append(bad_data)
         passed_datas = passed_datas.append(passed_data)
+        if end:
+            break
 
     if Output_CSV:
         datas.to_csv(Output_name[0], encoding=requests.get(Domain).apparent_encoding)
