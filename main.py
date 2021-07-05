152d62ca76aceae9f456b705bf74000ead237004
李一喆 <liyizhe@zwcad.com>
2021-07-05 16:50:14 +0800

    export txt

--- main.py
+++ main.py
@@ -3,8 +3,7 @@ import requests
 import time
 import json
 import re
-import os, sys
-import pandas as pd
+import csv
 from pyquery import PyQuery as pq
 
 Output_CSV = True
@@ -12,13 +11,13 @@ Output_name = ('out.csv', 'out_bad.csv', 'out_passed.csv')
 Domain = 'https://nyaa.si'
 Retry = 5
 
-allowed_author = ['jptv']
+allowed_author = ['jptv', '']
 allowed_category = []
 
 
 def get_last_time():
-    with open('config.json', 'r', encoding='utf-8') as load_f:
-        return datetime.datetime.strptime(json.load(load_f)['last-time'], "%Y-%m-%d %H:%M")
+    # with open('config.json', 'r', encoding='utf-8') as load_f:
+    return datetime.datetime.strptime("2019-06-28 00:01", "%Y-%m-%d %H:%M")
 
 
 def write_last_time():
@@ -37,6 +36,8 @@ def get_html(url):
             'Connection': 'close'
         }
         respond = requests.get(url, headers=headers)
+        if respond.status_code == 429:
+            return False
         requests.session().close()
         respond.encoding = respond.apparent_encoding
         return pq(respond.text)
@@ -75,81 +76,83 @@ def verify_item(item):
 
 
 def handle_table(trs, prefix, item_filter):
-    data = []
-    bad_data = []
-    passed_data = []
     bad = False
     passed = False
     end = False
     ite = 0
     num = len(trs)
+    fields = ['category', 'name', 'author', 'size', 'time', 'completes', 'magnet']
+    # with open("data.csv", "a") as data:
+    data = open("data.csv", "a", encoding='utf-8-sig', newline='')
+    data_w = csv.DictWriter(data, fieldnames=fields)
+    bad_data = open("bad_data.csv", "a", encoding='utf-8-sig', newline='')
+    bad_data_w = csv.DictWriter(bad_data, fieldnames=fields)
+    passed_data = open("passed_data.csv", "a", encoding='utf-8-sig', newline='')
+    passed_data_w = csv.DictWriter(passed_data, fieldnames=fields)
     for item in trs:
-        time.sleep(0.1)
-        ite += 1
-        tds = item('td')
-        href = prefix + tds[1][-1].attrib['href']
+        try:
+            time.sleep(2)
+            ite += 1
+            tds = item('td')
+            href = prefix + tds[1][-1].attrib['href']
 
-        author = get_author(href)
-        item = {
-            'category': tds[0][-1].attrib['title'],
-            'name': tds[1][-1].attrib['title'],
-            'author': author if author else "Nan",
-            'size': tds[-5].text,
-            'time': tds[-4].text,
-            'completes': int(tds[-1].text),
-            'magnet': tds[-6][-1].attrib['href']
-        }
-        if not verify_item(item):
-            bad_data.append(item)
-            bad = True
-        elif not check_time(item):
-            end = True
-            print("\033[1;33mReached last time.\033[0m")
-            break
-        elif not item_filter(item):
-            passed_data.append(item)
-            passed = True
-        else:
-            data.append(item)
+            author = get_author(href)
+            item = {
+                'category': tds[0][-1].attrib['title'].strip(),
+                'name': tds[1][-1].attrib['title'].strip(),
+                'author': author.strip() if author else "Nan",
+                'size': tds[-5].text,
+                'time': tds[-4].text,
+                'completes': int(tds[-1].text),
+                'magnet': tds[-6][-1].attrib['href']
+            }
+            if not verify_item(item):
+                bad_data_w.writerow(item)
+                bad = True
+            elif not check_time(item):
+                end = True
+                print("\033[1;33mReached last time.\033[0m")
+                break
+            elif not item_filter(item):
+                passed_data_w.writerow(item)
+                passed = True
+            else:
+                data_w.writerow(item)
 
-        print("\033[1;32m[{0}/{1}]\033[1;37m {2} \033[1;31m{3} \033[1;36m{4}\033[0m"
-              .format(ite, num, item['name'], "Bad" if bad else "", "Passed" if passed else ""))
-
-        bad = passed = False
-
-    data_frame = pd.DataFrame(data)
-    bad_data_frame = pd.DataFrame(bad_data)
-    passed_frame = pd.DataFrame(passed_data)
-    return data_frame, bad_data_frame, passed_frame, end
+            print("\033[1;32m[{0}/{1}]\033[1;37m {2} \033[1;31m{3} \033[1;36m{4}\033[0m"
+                  .format(ite, num, item['name'], "Bad" if bad else "", "Passed" if passed else ""))
+        except Exception as e:
+            pass
+        finally:
+            bad = passed = False
+    passed_data.close()
+    bad_data.close()
+    data.close()
+    return end
 
 
 def main(argv=None):
-    datas = pd.DataFrame(columns=('category', 'name', 'author', 'size', 'time', 'completes', 'magnet'))
-    bad_datas = pd.DataFrame(columns=('category', 'name', 'author', 'size', 'time', 'completes', 'magnet'))
-    passed_datas = pd.DataFrame(columns=('category', 'name', 'author', 'size', 'time', 'completes', 'magnet'))
-
-    for i in range(100):
+    docs = []
+    for i in range(1):
         print('\033[1;33mPage {0}:\033[0m'.format(i + 1))
-        doc = get_html(Domain + '/?p={0}'.format(i + 1))
-        if not doc:
-            print("Failed to fetch {0}th page.".format(i + 1))
-            break
+        try:
+            doc = get_html(Domain + '/?p={0}'.format(i + 1))
+            if not doc:
+                print("Failed to fetch {0}th page.".format(i + 1))
+                break
+            docs.append(doc)
+        except Exception as e:
+            pass
+    for i in range(len(docs)):
+        with open("doc_" + str(i + 1) + '.txt', "a", encoding='utf-8') as d:
+            d.write(docs[i].html())
+    for doc in docs:
         trs = list(doc.find('.torrent-list')('tbody')('tr').items())
-        data, bad_data, passed_data, end = handle_table(trs, Domain, lambda item: True)
-        datas = datas.append(data)
-        bad_datas = bad_datas.append(bad_data)
-        passed_datas = passed_datas.append(passed_data)
-        if end:
-            break
-
-    if Output_CSV:
-        datas.to_csv(Output_name[0], encoding=requests.get(Domain).apparent_encoding)
-        datas.to_csv(Output_name[1], encoding=requests.get(Domain).apparent_encoding)
-        datas.to_csv(Output_name[2], encoding=requests.get(Domain).apparent_encoding)
+        end = handle_table(trs, Domain, lambda item: True)
+        # if end:
+        #     break
 
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     main()
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
